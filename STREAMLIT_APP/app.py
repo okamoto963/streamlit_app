6b49703e8dbbccbb00fb51eda717dd538d578798
okamoto963 <T125026@ed.sus.ac.jp>
2026-01-26 11:45:00 +0900

    1st commit

--- STREAMLIT_APP/app.py
+++ STREAMLIT_APP/app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 import pandas as pd
+from pandas import DataFrame as df
+import plotly as plt
+import numpy as np
 
+st.title('test')
+pd.read_csv('STREAMLIT_APP\ma030000.csv')
+pd.read_csv('STREAMLIT_APP\表6-2.csv')
 
-st.write('test')
+df.head()
+df.shape
+df.dtypes
+df.isna().sum()
