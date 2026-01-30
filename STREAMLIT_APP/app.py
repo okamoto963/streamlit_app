59d916dc9c666be69f347af23b8965bed43ec08e
okamoto963 <T125026@ed.sus.ac.jp>
2026-01-31 04:55:00 +0900

    0131

--- STREAMLIT_APP/app.py
+++ STREAMLIT_APP/app.py
@@ -4,9 +4,10 @@ from pandas import DataFrame as df
 import plotly as plt
 import numpy as np
 
-st.title("都道府県別の人口と交通事故件数の関係")
+st.title("tameshi")
 
-df1 = pd.read_csv("令和6年人口動態統計.csv", encoding="shift_jis")
-df2 = pd.read_csv("表6-2.csv",encoding="shift_jis")
+df1 = pd.read_csv("CPI.csv", encoding="shift_jis")
 
-with st.sidebar:
+#st.table(df1)  CSVが読み込めているか確認
+
+#with st.sidebar:
