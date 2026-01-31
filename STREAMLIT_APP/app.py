1dccab6f3bb48c8aab596842d9dce5784ad93a38
okamoto963 <T125026@ed.sus.ac.jp>
2026-02-01 02:01:54 +0900

    0201

--- STREAMLIT_APP/app.py
+++ STREAMLIT_APP/app.py
@@ -1,13 +1,18 @@
 import streamlit as st
 import pandas as pd
-from pandas import DataFrame as df
-import plotly as plt
-import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams['font.family'] = 'MS Gothic'
+matplotlib.rcParams['axes.unicode_minus'] = False
+
 
 st.title("CPI可視化app")
 
 df = pd.read_csv("CPI.csv", encoding="shift_jis")
 
+
+
+# 必須構成　その１　(アプリの概要・目的・使い方)
 st.subheader("CPIとは")
 st.write("CPIとは、消費者が購入する商品やサービスの価格の変動を測定する経済指標を表しています。")
 
@@ -15,17 +20,45 @@ st.subheader('概要と目的')
 st.write('このアプリは、品目ごとの価格変動をグラフにして可視化することで経済の背景を学ぶことを目的としています。')
 
 st.subheader('使い方')
-st.write('はじめにCPIの変動を調べたい品目を選択してください。これにより選択した品目のCPIの変動をグラフで表示します。選択した品目が1つである場合、選択した品目の棒グラフ及び折れ線グラフを、選択した品目が複数ある場合、選択した品目全ての折れ線グラフを表示します。')
-#st.table(df1)  # CSVが読み込めているか確認
+st.write('はじめにCPIの変動を調べたい品目を選択してください。',
+         '選択した品目が1つである場合、選択した品目の棒グラフ及び折れ線グラフを、',
+         '選択した品目が複数ある場合、選択した品目全ての折れ線グラフを表示します。')
 
-# アプリの概要
-# 
+#st.table(df)  # CSVが読み込めているか確認
+
 
 with st.sidebar:
   items = st.multiselect(
      '表示したい項目を選んでください(複数選択可能)',
-     df.columns.tolist()
+     df.columns[2:]
    )
-  
-if items:
-   st.dataframe(df[items])
+# UI その１  
+if not items:
+   st.warning('品目を1つ以上選択してください(複数選択可)')
+   st.stop()
+# 1つのみ選択(2種類のグラフ表示)
+if len(items) == 1:
+    item = items[0]
+    fig, ax = plt.subplots(figsize=(12, 6))
+
+    ax.plot(df['年'], df[item], marker='o')
+    ax.bar(df['年'], df[item], alpha=0.6)
+
+    ax.set_xlabel("年")
+    ax.set_ylabel("指数")
+    ax.grid(True)
+    
+    st.pyplot(fig) # グラフの表示
+
+elif len(items) >= 2:
+    fig, ax = plt.subplots(figsize=(12, 6))
+
+    for item in items:
+        ax.plot(df['年'], df[item], marker='o', label=item)
+
+    ax.set_xlabel("年")
+    ax.set_ylabel("指数")
+    ax.grid(True)
+    ax.legend()
+
+    st.pyplot(fig)
